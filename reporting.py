0bc418df5e4743b7f68cfb141c856b961e4f6a34
Janusz Lenar <malleor@users.noreply.github.com>
2016-02-29 23:26:24 +0100

    Update reporting.py

--- reporting.py
+++ reporting.py
@@ -1,5 +1,7 @@
 import os
 import urllib as url
+import re
+import numpy as np
 
 
 _TOKEN = os.environ.get('TRELLO_TOKEN')
@@ -18,3 +20,21 @@ def _get_lists(board_id):
 
 def _get_cards(board_id):
   return [c['name'] for c in _get_from_trello('boards/%s/cards' % board_id, 'fields=name')]
+  
+  
+def _get_time_data(cards):
+  def cutout(card, pattern):
+    m = re.search(pattern, card)
+    if m is None:
+      return ''
+    b, e = m.span()
+    return card[b:e]
+    
+  def parse(time):
+    if time in ['', '?']:
+      return np.nan
+    return float(time)
+    
+  get_est  = lambda card: parse(cutout(card, r'^\(.*?\)')[1:-1])
+  get_real = lambda card: parse(cutout(card, r'\[.*?\]$')[1:-1])
+  return np.array([[get_est(card), get_real(card)] for card in cards])
